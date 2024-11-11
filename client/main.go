f97179b184e1c949630f174b4489a93cda84eb2a
midays <midays@redhat.com>
2024-11-11 17:59:17 +0200

    fixed client

--- client/main.go
+++ client/main.go
@@ -9,43 +9,55 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var listFlag bool
+var (
+	getByIDArg string
+	listFlag   bool
+	baseURL    = "http://127.0.0.1:8080/"
+)
 
 func main() {
 	var rootCMD = &cobra.Command{
 		Use:   "todo-client",
 		Short: "A simple CLI client to interact with the server",
 		Run: func(cmd *cobra.Command, args []string) {
+
 			if listFlag {
+				url := baseURL + "todo/getAll"
+				sendGetRequest(url)
+			}
 
-				req, err := http.NewRequest("GET", "http://127.0.0.1:8080/todo/getAll/", nil)
-				if err != nil {
-					log.Fatalf("Failed to create request: %v", err)
-				}
-
-				client := &http.Client{}
-				response, err := client.Do(req)
-				if err != nil {
-					log.Fatalf("Failed to fetch todos: %v", err)
-				}
-
-				defer response.Body.Close()
-
-				body, err := ioutil.ReadAll(response.Body)
-
-				if err != nil {
-					log.Fatalf("Failed to read response body: %v", err)
-				}
-
-				fmt.Println(string(body))
-
+			if getByIDArg != "" {
+				url := baseURL + "todo/getByID/" + getByIDArg
+				sendGetRequest(url)
+			}
+			if !listFlag && getByIDArg == "" {
+				fmt.Print("A simple CLI client to interact with the server\n")
+				fmt.Print("Usage:\n")
+				fmt.Print("\t--list - to list all todo items\n")
+				fmt.Print("\t--getById <id> - to get a specific item\n")
 			}
 		},
 	}
 
 	rootCMD.Flags().BoolVar(&listFlag, "list", false, "List all todo items")
+	rootCMD.Flags().StringVar(&getByIDArg, "getById", "", "Get a todo item by ID")
 
 	if err := rootCMD.Execute(); err != nil {
 		log.Fatalf("Error executing command: %v", err)
 	}
+}
+
+func sendGetRequest(url string) {
+	response, err := http.Get(url)
+	if err != nil {
+		log.Fatalf("Failed to fetch data: %v", err)
+	}
+	defer response.Body.Close()
+
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		log.Fatalf("Failed to read response body: %v", err)
+	}
+
+	fmt.Println(string(body))
 }
